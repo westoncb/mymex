ea84b7e887b243537d681854b7faa1a3df9cbb4c
Weston C. Beecroft <westoncb@gmail.com>
2020-03-05 13:24:11 -0700

    Actually working AnnotationsPanel with hooks

--- src/AnnotationsPanel.js
+++ src/AnnotationsPanel.js
@@ -1,48 +1,49 @@
-import React, {PureComponent} from "react"
+import React, { useState, useEffect } from "react"
 import './AnnotationsPanel.css'
 import { TagInput, EditableText} from "@blueprintjs/core"
 import { IconNames } from "@blueprintjs/icons";
 import DataStore from './DataStore'
 
-class AnnotationsPanel extends PureComponent {
+export function AnnotationsPanel(props) {
+    const [tags, setTags] = useState([])
+    const [notes, setNotes] = useState("")
 
-    constructor(props) {
-        super(props)
+    useEffect(() => {
+        const func = async () => {
+            const mem = await DataStore.getMem(props.mem._id)
+            setTags(mem.tags)
+            setNotes(mem.notes)
+        }
+        func()
+    }, [props.mem._id])
 
-        this.state = { tags: this.props.mem.tags || [], notes: this.props.mem.notes || ""}
+    const handleTagChange = tags => {
+        setTags(tags)
+        DataStore.setMemTags(props.mem._id, tags)
+    }
+    const handleNotesTextChange = notes => {
+        setNotes(notes)
+        DataStore.setMemNotes(props.mem._id, notes)
     }
 
-    handleTagChange = tags => {
-        this.setState({tags})
-        DataStore.setMemTags(this.props.mem._id, tags)
-    }
-    handleNotesTextChange = notes => {
-        this.setState({ notes })
-        DataStore.setMemNotes(this.props.mem._id, notes)
-    }
-
-    render() {
-        const { tags } = this.state
-
-        return (
-            <div className="annotations-panel bp3-dark">
-                <EditableText
-                    multiline
-                    alwaysRenderInput
-                    placeholder="Write notes here"
-                    minLines={1}
-                    maxLines={5}
-                    onChange={this.handleNotesTextChange}
-                    value={this.state.notes}
-                />
-                <TagInput
-                    leftIcon={IconNames.TAG}
-                    values={this.state.tags}
-                    onChange={this.handleTagChange}
-                />
-            </div>
-        )
-    }
+    return (
+        <div className="annotations-panel bp3-dark">
+            <EditableText
+                multiline
+                alwaysRenderInput
+                placeholder="Write notes here"
+                minLines={1}
+                maxLines={5}
+                onChange={handleNotesTextChange}
+                value={notes}
+            />
+            <TagInput
+                leftIcon={IconNames.TAG}
+                values={tags}
+                onChange={handleTagChange}
+            />
+        </div>
+    )
 }
 
 export default AnnotationsPanel
