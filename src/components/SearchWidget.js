1700707f45a091557086666a8ecc6a4c26b6b304
Weston C. Beecroft <westoncb@gmail.com>
2020-03-06 16:23:49 -0700

    Convert SearchWidget to function component

--- src/components/SearchWidget.js
+++ src/components/SearchWidget.js
@@ -1,52 +1,29 @@
-import React, { PureComponent } from "react";
+import React, { useState } from "react";
 import DataSourceDialog from './DataSourceDialog';
 import './SearchWidget.css'
 import { Icon, Button } from "@blueprintjs/core";
 import { IconNames } from "@blueprintjs/icons";
 
-export default class SearchWidget extends PureComponent {
-    constructor(props) {
-        super(props);
-        this.state = {
-            inputFocused: false,
-            isDialogOpen: false
-        }
+export default function SearchWidget(props) {
 
-        this.handleFocus = this.handleFocus.bind(this)
-        this.handleBlur = this.handleBlur.bind(this)
-        this.handleTextChange = this.handleTextChange.bind(this)
+    const [isDialogOpen, setIsDialogOpen] = useState(false)
+    const textInput = React.createRef()
 
-        this.textInput = React.createRef();        
+    const handleTextChange = e => {
+        const string =  e.target.value || ""
+        props.updateSearchString(string)
     }
 
-    handleFocus(e) {
-        this.setState({ inputFocused: true })
-    }
-
-    handleBlur(e) {
-        // this.setState({ inputFocused: false })
-    }
-
-   handleTextChange(e) {
-      const string =  e.target.value || ""
-      this.props.updateSearchString(string)
-   }
-
-   handleOpen = () => this.setState({ isDialogOpen: true })
-   handleClose = () => this.setState({ isDialogOpen: false })
-
-    render() {
-        return (
-            <div className="search-widget-container" onFocus={this.handleFocus} onBlur={this.handleBlur}>
-                <div className="input-results-group">
-                  <input autoFocus className="search-input" type="text" ref={this.textInput} onChange={this.handleTextChange} />
-                </div>
-
-              <Button style={{flexShrink: 0}} onClick={this.handleOpen}>
-                 <Icon icon={IconNames.DIAGRAM_TREE} iconSize={42} />
-              </Button>
-              <DataSourceDialog isOpen={this.state.isDialogOpen} handleClose={this.handleClose}></DataSourceDialog>
+    return (
+        <div className="search-widget-container">
+            <div className="input-results-group">
+                <input autoFocus className="search-input" type="text" ref={textInput} onChange={handleTextChange} />
             </div>
-        )
-    }
+
+            <Button style={{ flexShrink: 0 }} onClick={() => setIsDialogOpen(true)}>
+                <Icon icon={IconNames.DIAGRAM_TREE} iconSize={42} />
+            </Button>
+            <DataSourceDialog isOpen={isDialogOpen} handleClose={() => setIsDialogOpen(false)}></DataSourceDialog>
+        </div>
+    )
 }
