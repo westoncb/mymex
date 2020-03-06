a76351fa0749397879af662cefd2c27fa6ce6cbb
Weston C. Beecroft <westoncb@gmail.com>
2020-03-06 16:25:23 -0700

    Remove unused text input ref

--- src/components/SearchWidget.js
+++ src/components/SearchWidget.js
@@ -7,7 +7,6 @@ import { IconNames } from "@blueprintjs/icons";
 export default function SearchWidget(props) {
 
     const [isDialogOpen, setIsDialogOpen] = useState(false)
-    const textInput = React.createRef()
 
     const handleTextChange = e => {
         const string =  e.target.value || ""
@@ -17,7 +16,7 @@ export default function SearchWidget(props) {
     return (
         <div className="search-widget-container">
             <div className="input-results-group">
-                <input autoFocus className="search-input" type="text" ref={textInput} onChange={handleTextChange} />
+                <input autoFocus className="search-input" type="text" onChange={handleTextChange} />
             </div>
 
             <Button style={{ flexShrink: 0 }} onClick={() => setIsDialogOpen(true)}>
