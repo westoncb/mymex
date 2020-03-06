66e57a1249f554b9cb66db2fa448141fc8617530
Weston C. Beecroft <westoncb@gmail.com>
2020-03-06 15:57:01 -0700

    Convert DataSourceDialog to function component

--- src/components/DataSourceDialog.js
+++ src/components/DataSourceDialog.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from "react";
+import React, { useState, useEffect } from "react";
 import DataStore from "../DataStore"
 import { Icon, Button, Dialog, Classes, AnchorButton, NonIdealState, Divider, ControlGroup, RadioGroup, Radio } from "@blueprintjs/core";
 import { IconNames } from "@blueprintjs/icons";
@@ -9,92 +9,83 @@ const electron = window.require('electron').remote
 const dialog = electron.dialog
 
 
-class DataSourceDialog extends PureComponent {
-    constructor(props) {
-        super(props)
+export default function DataSourceDialog(props) {
+    const [selectedSourceType, setSelectedSourceType] = useState(C.DS_TYPE_CHROME)
+    const [dataSources, setDataSources] = useState([])
 
-        this.state = {
-            selectedSourceType: C.DS_TYPE_CHROME,
-            dataSources: []
-        }
-
-        DataStore.getDataSources(dataSources => {
-            this.setState({dataSources})
+    useEffect(() => {
+        DataStore.getDataSources(freshDataSources => {
+            setDataSources(freshDataSources)
         })
-    }
+    }, [])
 
-    handleSourceTypeChange = event => this.setState({ selectedSourceType: event.target.value })
+    const handleSourceTypeChange = event => setSelectedSourceType(event.target.value)
 
-    handleAddSourceButton = () => {
-        const type = this.state.selectedSourceType
+    const handleAddSourceButton = () => {
         let resultPromise
 
-        if (type === C.DS_TYPE_CHROME) {
+        if (selectedSourceType === C.DS_TYPE_CHROME) {
             const defaultPath = path.join(electron.app.getPath('home'), "/Library/Application Support/Google/Chrome/Default")
             resultPromise = dialog.showOpenDialog({title: "Select Chrome bookmarks file", defaultPath, properties: ['openFile'] })
-        } else if (type === C.DS_TYPE_DIRECTORY) {
+        } else if (selectedSourceType === C.DS_TYPE_DIRECTORY) {
             resultPromise = dialog.showOpenDialog({title: "Choose folder", properties: ['openDirectory', 'showHiddenFiles', 'createDirectory'] })
         }
 
         resultPromise.then((result, err) => {
             if (!result.canceled) {
                 const path = result.filePaths[0]
-                DataStore.addDataSource({ name: type, path, type }, dataSource => {
+                DataStore.addDataSource({ name: selectedSourceType, path, selectedSourceType }, dataSource => {
                     console.log("dataSources", dataSource)
-                    this.setState({ dataSources: this.state.dataSources.concat(dataSource) })
+                    setDataSources(dataSources.concat(dataSource))
                 })
             }
         })
     }
 
-    render() {
-        let mainSection
+    let mainSection
 
-        if (this.state.dataSources.length === 0) {
-            mainSection = <NonIdealState
-                icon={IconNames.DIAGRAM_TREE}
-                title="No data sources"
-                description="No data sources have been added yet"
-            />
-        } else {
-            mainSection = 
-                <div className="data-source-list">
-                {this.state.dataSources.map(ds => <div className="data-source-item" key={ds._id}>{ds.name}:{ds.path}</div>)}
-                </div>
-        }
-
-        return (
-            <Dialog
-                className="bp3-dark"
-                icon={IconNames.DIAGRAM_TREE}
-                onClose={this.handleClose}
-                title="Manage data sources"
-                isOpen={this.props.isOpen}
-            >
-                <div className={Classes.DIALOG_BODY}>
-                    {mainSection}
-                    <Divider/>
-                    <ControlGroup className="data-source-control-group" fill={true}>
-                        <RadioGroup
-                            label="Data source type"
-                            onChange={this.handleSourceTypeChange}
-                            selectedValue={this.state.selectedSourceType}
-                        >
-                            <Radio label="Chrome bookmarks file" value={C.DS_TYPE_CHROME}></Radio>
-                            <Radio label="File system location" value={C.DS_TYPE_DIRECTORY}></Radio>
-                        </RadioGroup>
-                        <Button onClick={this.handleAddSourceButton}><Icon icon={IconNames.ADD} iconSize={42}/></Button>
-                    </ControlGroup>
-                </div>
-
-                <div className={Classes.DIALOG_FOOTER}>
-                    <div className={Classes.DIALOG_FOOTER_ACTIONS}>
-                        <AnchorButton onClick={this.props.handleClose}>Done</AnchorButton>
-                    </div>
-                </div>
-            </Dialog>
-        )
+    if (dataSources.length === 0) {
+        mainSection = <NonIdealState
+            icon={IconNames.DIAGRAM_TREE}
+            title="No data sources"
+            description="No data sources have been added yet"
+        />
+    } else {
+        mainSection =
+            <div className="data-source-list">
+                {dataSources.map(ds => <div className="data-source-item" key={ds._id}>{ds.name}:{ds.path}</div>)}
+            </div>
     }
-}
 
-export default DataSourceDialog
+    return (
+        <Dialog
+            className="bp3-dark"
+            icon={IconNames.DIAGRAM_TREE}
+            onClose={props.handleClose}
+            title="Manage data sources"
+            isOpen={props.isOpen}
+        >
+            <div className={Classes.DIALOG_BODY}>
+                {mainSection}
+                <Divider />
+                <ControlGroup className="data-source-control-group" fill={true}>
+                    <RadioGroup
+                        label="Data source type"
+                        onChange={handleSourceTypeChange}
+                        selectedValue={selectedSourceType}
+                    >
+                        <Radio label="Chrome bookmarks file" value={C.DS_TYPE_CHROME}></Radio>
+                        <Radio label="File system location" value={C.DS_TYPE_DIRECTORY}></Radio>
+                    </RadioGroup>
+                    <Button onClick={handleAddSourceButton}><Icon icon={IconNames.ADD} iconSize={42} /></Button>
+                </ControlGroup>
+            </div>
+
+            <div className={Classes.DIALOG_FOOTER}>
+                <div className={Classes.DIALOG_FOOTER_ACTIONS}>
+                    <AnchorButton onClick={props.handleClose}>Done</AnchorButton>
+                </div>
+            </div>
+        </Dialog>
+    )
+}
