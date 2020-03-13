c39b6ab00f403686b499db8f84f0bf4766a102ef
Weston C. Beecroft <westoncb@gmail.com>
2020-03-12 18:04:24 -0700

    Show tags in MiniAnnotations

--- src/components/MiniAnnotations.js
+++ src/components/MiniAnnotations.js
@@ -1,15 +1,30 @@
 import React from "react"
 import './MiniAnnotations.css'
-import { Icon } from "@blueprintjs/core"
+import { Icon, OverflowList, Classes, Tag, Boundary } from "@blueprintjs/core"
 import { IconNames } from "@blueprintjs/icons";
 
 export default function MiniAnnotations(props) {
+
+
 
     return (
         <div className="mini-annotations">
 
             {props.mem.notes &&
-                <Icon style={{ color: "#ccc"}} icon={IconNames.COMMENT} iconSize={16}/>
+                <Icon style={{ color: "#ffed66", marginRight: "0.5rem"}} icon={IconNames.COMMENT} iconSize={20}/>
+            }
+
+            {props.mem.tags && 
+                <OverflowList
+                    items={props.mem.tags}
+                    overflowRenderer={overflowItems => {
+                        return <span className={Classes.BREADCRUMBS_COLLAPSED} />
+                    }}
+                    visibleItemRenderer={(item, index) => {
+                        return <Tag className="mini-tag" key={index}>{item}</Tag>
+                    }}
+                    collapseFrom={Boundary.END}
+                />
             }
         </div>
     )
